1f88bfae83df0e1565cdf389b6ac5401e5c6a341
rajeshpalaniswami <rajesh@sideqik.com>
2025-06-10 10:13:15 +0530

    Jun_10_2025 Task updated

--- src/ex_02_Java_Basics/Lab_Assignment.java
+++ src/ex_02_Java_Basics/Lab_Assignment.java
@@ -4,27 +4,19 @@ public class Lab_Assignment {
     public static void main(String[] args) {
         //printing of 9 table using printf
         int a = 9;
-
-
-        System.out.printf("%d * 1 = "+a*1,a);
-        System.out.println();
-        System.out.printf("%d * 2 = "+a*2,a);
-        System.out.println();
-        System.out.printf("%d * 3 = "+a*3,a);
-        System.out.println();
-        System.out.printf("%d * 4 = "+a*4,a);
-        System.out.println();
-        System.out.printf("%d * 5 = "+a*5,a);
-        System.out.println();
-        System.out.printf("%d * 6 = "+a*6,a);
-        System.out.println();
-        System.out.printf("%d * 7 = "+a*7,a);
-        System.out.println();
-        System.out.printf("%d * 8 = "+a*8,a);
-        System.out.println();
-        System.out.printf("%d * 9 = "+a*9,a);
-        System.out.println();
-        System.out.printf("%d * 10 = "+a*10,a);
+        char new_line = '\n';
+        System.out.println("Printing of nine table using printf");
+        System.out.println("************************************************");
+        System.out.printf("%d * 1 = "+a*1+new_line,a);
+        System.out.printf("%d * 2 = "+a*2+new_line,a);
+        System.out.printf("%d * 3 = "+a*3+new_line,a);
+        System.out.printf("%d * 4 = "+a*4+new_line,a);
+        System.out.printf("%d * 5 = "+a*5+new_line,a);
+        System.out.printf("%d * 6 = "+a*6+new_line,a);
+        System.out.printf("%d * 7 = "+a*7+new_line,a);
+        System.out.printf("%d * 8 = "+a*8+new_line,a);
+        System.out.printf("%d * 9 = "+a*9+new_line,a);
+        System.out.printf("%d * 10 = "+a*10+new_line,a);
 
     }
 }
