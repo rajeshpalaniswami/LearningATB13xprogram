6b35f208bfaf974b3b2618dd6629c96416a4e6bd
rajeshpalaniswami <rajesh@sideqik.com>
2025-06-10 08:46:12 +0530

    Jun_9_2025 Class

---
+++ src/ex_01_Java_Basics/Lab003_NoMainMethod.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab003_NoMainMethod {
+    public static void main(String[] args) {
+        //This is the class without main method
+    /*
+    without main method we can not run the program
+    main method is the starting point of the program
+     */
+    }
+
+}
---
+++ src/ex_01_Java_Basics/Lab004_Comments.java
@@ -0,0 +1,22 @@
+package ex_01_Java_Basics;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+
+        // This is a comment, this code will nto be executed.
+         /*System.out.println("Hi");
+         System.out.println("Bye");
+         System.out.println("Drink");
+         System.out.println("Chai");*/
+
+        // single line comments
+    /*
+    Multiple line comments
+    1
+    2
+    3
+
+     */
+    }
+
+}
