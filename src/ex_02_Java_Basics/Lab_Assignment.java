83e743c2015cece5a1d2f1a21d27dc39d6dda8dc
rajeshpalaniswami <rajesh@sideqik.com>
2025-06-10 09:51:09 +0530

    Jun_10_2025 Task

---
+++ src/ex_02_Java_Basics/Lab_Assignment.java
@@ -0,0 +1,30 @@
+package ex_02_Java_Basics;
+
+public class Lab_Assignment {
+    public static void main(String[] args) {
+        //printing of 9 table using printf
+        int a = 9;
+
+
+        System.out.printf("%d * 1 = "+a*1,a);
+        System.out.println();
+        System.out.printf("%d * 2 = "+a*2,a);
+        System.out.println();
+        System.out.printf("%d * 3 = "+a*3,a);
+        System.out.println();
+        System.out.printf("%d * 4 = "+a*4,a);
+        System.out.println();
+        System.out.printf("%d * 5 = "+a*5,a);
+        System.out.println();
+        System.out.printf("%d * 6 = "+a*6,a);
+        System.out.println();
+        System.out.printf("%d * 7 = "+a*7,a);
+        System.out.println();
+        System.out.printf("%d * 8 = "+a*8,a);
+        System.out.println();
+        System.out.printf("%d * 9 = "+a*9,a);
+        System.out.println();
+        System.out.printf("%d * 10 = "+a*10,a);
+
+    }
+}
